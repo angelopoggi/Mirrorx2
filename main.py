501c4a15ca8b28e136cc9e64e4020d082aaf5bbb
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-10-01 15:59:53 -0400

    testing

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #2022 Angelo Poggi
 #angelo.poggi@enigmaconsulting.tech
 
+import os
+
 from src.mirror_speech import MirrorSpeech
 from src.mirror_audio import MirrorAudio
+from src.mirror_dream import MirrorDream
 
 if __name__ == "__main__":
     record = MirrorAudio()
     speak = MirrorSpeech()
+    dream = MirrorDream()
     record.mirror_record()
     speak.mirror_speech("recordedFile.wav")
+    with open("prompt.txt", 'r') as prompt:
+        input_prompt = prompt.readlines()
+        dream.dream(input_prompt)
+
+    os.system(f"fbi -a generated_img.jpg")
 
---
+++ src/mirror_dream.py
@@ -0,0 +1,31 @@
+import io
+import warnings
+
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+from PIL import Image
+
+
+
+class MirrorDream()
+    def __init__(self):
+        pass
+    def dream(self, prompt):
+        stability_api = client.StabilityInference(
+            key='STABILITY_API_KEY',
+            verbose=True,
+        )
+        answers = stability_api.generate(
+            prompt=prompt
+        )
+        for resp in answers:
+            for artifact in resp.artifacts:
+                if artifact.finish_reason == generation.FILTER:
+                    warnings.warn(
+                        "Your request activated the API's safety filters and could not be processed."
+                        "Please modify the prompt and try again.")
+                    if artifact.type == generation.ARTIFACT_IMAGE:
+                        img = Image.open(io.BytesIO(artifact.binary))
+                        img.save(fp='generated_image.jpg')
+
+
