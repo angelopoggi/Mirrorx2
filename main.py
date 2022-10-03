4bad9023cce631f3c3890597985854355197b91f
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-10-03 00:11:41 -0400

    testing

--- main.py
+++ main.py
@@ -6,22 +6,16 @@ import os
 from src.mirror_speech import MirrorSpeech
 from src.mirror_audio import MirrorAudio
 from src.mirror_dream import MirrorDream
-from utils.rand_util import RandUtils
+
 
 if __name__ == "__main__":
     record = MirrorAudio()
     speak = MirrorSpeech()
     dream = MirrorDream()
     record.mirror_record()
-    rand = RandUtils()
-    prompt = rand.rand_prompt()
-    print(prompt)
     speak.mirror_speech("recordedFile.wav")
     with open("prompt.txt", 'r') as prompt:
         input_prompt = prompt.readlines()
-        print(input_prompt)
-        input_prompt = f"{input_prompt[0]},{prompt['medium']}, by {prompt['artist']}"
-        print(input_prompt)
         dream.dream(input_prompt)
 
     os.system(f"fbi -a {prompt}.jpg")
--- src/mirror_speech.py
+++ src/mirror_speech.py
@@ -1,3 +1,5 @@
+from utils.rand_util import RandUtils
+
 import speech_recognition as sr
 
 class MirrorSpeech():
@@ -6,11 +8,12 @@ class MirrorSpeech():
     
     def mirror_speech(self,filename):
         r = sr.Recognizer()
+        rand = RandUtils()
         with sr.AudioFile(filename) as source:
             # listen for the data (load audio to memory)
             audio_data = r.record(source)
             # recognize (convert from speech to text)
             text = r.recognize_google(audio_data)
+            rand_prompt = rand.rand_prompt()
             with open('prompt.txt', 'w') as prompt_file:
-                prompt_file.write(text)
-
+                prompt_file.write(f"{text}, {rand_prompt['medium']}, by {rand_prompt['artist']}")
